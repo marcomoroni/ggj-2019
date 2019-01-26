56ae935efd099a3faad037c46f107f4b712e8344
Marco Moroni <marcomoroni394@gmail.com>
2019-01-26 16:33:15 +0000

    Add many sprites

---
+++ Global Game Jam 2019/Assets/NPC/PlayerInteractionArea.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractionArea : MonoBehaviour
+{
+	public GameObject player;
+	public DialogueID dialogueToStart;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject == player)
+		{
+
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject == player)
+		{
+
+		}
+	}
+}
