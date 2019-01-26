8bf08fb6d20bca0998f744acdfa6fc38196c1a51
Marco Moroni <marcomoroni394@gmail.com>
2019-01-26 14:31:03 +0000

    Add home virtual camera

---
+++ Global Game Jam 2019/Assets/CameraSnap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSnap : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject snapCamera;
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject == player)
+		{
+			snapCamera.SetActive(true);
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject == player)
+		{
+			snapCamera.SetActive(false);
+		}
+	}
+}
