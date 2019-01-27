0d0162dbb9e3fa4d4dda4f8407e8e440d00eddbc
Marco Moroni <marcomoroni394@gmail.com>
2019-01-27 11:24:54 +0000

    Small changes

--- Global Game Jam 2019/Assets/Main Game Manager/MainGameManager.cs
+++ Global Game Jam 2019/Assets/Main Game Manager/MainGameManager.cs
@@ -35,7 +35,7 @@ public class MainGameManager : FiniteStateMachine
 
 	private IEnumerator IntroCutscene()
 	{
-		yield return new WaitForSeconds(1.0f);
+		yield return new WaitForSeconds(2.0f);
 
 		dialogueManager.StartDialogue(DialogueID.Intro, true);
 
